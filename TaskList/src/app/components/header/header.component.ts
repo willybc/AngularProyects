557dad398eafdfe07feffbcda1acbcbd6bc7ca32
willybc <willybc06@gmail.com>
2022-07-18 19:49:36 -0300

    new service ui

--- TaskList/src/app/components/header/header.component.ts
+++ TaskList/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: String = 'Mi lista de Tareas';
+  showAddTask: boolean = false;
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor(
+    private uiService: UiService
+  ) {
+    this.subscription = this.uiService.onToggle()
+      .subscribe(value => this.showAddTask = value)
+   }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-    console.log("toggleAddTask!");
+    this.uiService.toggleAddTask();
   }
 }
