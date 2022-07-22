8f5b3fc5020794d4338a22c2efcbe698bfba64ac
willybc <willybc06@gmail.com>
2022-07-22 16:42:49 -0300

    News componets

--- IntiAPP/src/app/app.module.ts
+++ IntiAPP/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { AchievementsComponent } from './components/achievements/achievements.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ExperienceComponent,
+    AptitudesComponent,
+    AchievementsComponent
   ],
   imports: [
     BrowserModule,
---
+++ IntiAPP/src/app/components/aptitudes/aptitudes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aptitudes',
+  templateUrl: './aptitudes.component.html',
+  styleUrls: ['./aptitudes.component.sass']
+})
+export class AptitudesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
