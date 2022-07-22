331dd19a412270387d335d4e2dd2d5a999f8f0ea
willybc <willybc06@gmail.com>
2022-07-22 19:20:24 -0300

    Data binding - Interpolacion

--- IntiAPP/src/app/components/certifications/certifications.component.ts
+++ IntiAPP/src/app/components/certifications/certifications.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-certifications',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certifications.component.sass']
 })
 export class CertificationsComponent implements OnInit {
+  certificateList: any;
 
-  constructor() { }
+  constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.certificateList = data.certificates;
+    })
   }
 
 }
